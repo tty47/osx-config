200ccc595057774db553d86c69ec4c9a4e7976d9
Jose Ramon Mañes <jr.mb47@gmail.com>
2022-09-29 23:40:14 +0200

    migreate treesitter config from init.vom

--- files/nvim_lua/after/plugin/treesitter.rc.lua
+++ files/nvim_lua/after/plugin/treesitter.rc.lua
@@ -34,7 +34,20 @@ ts.setup {
   autotag = {
     enable = true,
   },
+  custom_captures = {
+    ["foo.bar"] = "Identifier",
+  },
+  additional_vim_regex_highlighting = false,
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
