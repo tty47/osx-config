fa95b3b990a7dbcef578204a57b4b94d2b98a42b
Jose Ramon Mañes <jr.mb47@gmail.com>
2022-12-07 18:48:51 +0100

    Theme NightFox

--- files/nvim/lua/jrmanes/colorscheme.lua
+++ files/nvim/lua/jrmanes/colorscheme.lua
@@ -1,14 +1,18 @@
+-- Themes Config
+-- ---------------------------------------------------------------------
 -- TokyoNight
-vim.cmd("colorscheme tokyonight")
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_transparent = true
-vim.g.tokyonight_transparent_sidebar = true
+-- vim.cmd("colorscheme tokyonight")
+-- vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_italic_functions = true
+-- vim.g.tokyonight_transparent = true
+-- vim.g.tokyonight_transparent_sidebar = true
 
--- require('nightfox').setup({
---   options = {
---     -- Compiled file's destination location
---     transparent = true,    -- Disable setting background
---   }
--- })
--- vim.cmd("colorscheme nightfox")
+-- ---------------------------------------------------------------------
+-- NightFox
+require('nightfox').setup({
+  options = {
+    -- Compiled file's destination location
+    transparent = true, -- Disable setting background
+  }
+})
+vim.cmd("colorscheme nightfox")
